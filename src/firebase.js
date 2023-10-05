10fb1b0c2581ebcace6585ab15716b606b12db75
Karbasappa B <karbasappabembulge@gmail.com>
2023-10-05 12:04:35 +0530

    Major changes: user can change the status and some more stats user can filter according to status of list

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAzUCh5byvDkQWATSbjsgSSejR-XD5ock0",
+  authDomain: "to-do-list-basu.firebaseapp.com",
+  projectId: "to-do-list-basu",
+  storageBucket: "to-do-list-basu.appspot.com",
+  messagingSenderId: "948572253015",
+  appId: "1:948572253015:web:077b9d5ac78b2448daec12",
+};
